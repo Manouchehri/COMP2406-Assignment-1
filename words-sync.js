a950fd8c2429eafca273488977b8088202caaabb
David Manouchehri <david@davidmanouchehri.com>
2016-01-17 02:52:48 +0000

    Program is working, looks good enough.

---
+++ words-sync.js
@@ -0,0 +1,42 @@
+var fs = require("fs");
+
+if(process.argv.length != 4) {
+    console.log("What on earth are you doing?");
+    console.log("Usage: node words-sync.js document.txt wordlist.txt");
+    process.exit(1337);
+}
+
+var doc_file = process.argv[2];
+var word_list = process.argv[3];
+
+var raw_doc_file = fs.readFileSync(doc_file, "UTF-8");
+var raw_doc_file_split = raw_doc_file.split("\n");
+
+// Can't get this to work.
+// var word_splitter = new RegExp("/\W//");
+
+var holder = [];
+
+for(var i = 0; i < raw_doc_file_split.length; i++) {
+    holder = holder.concat(raw_doc_file_split[i].split(" "));
+}
+
+holder.sort();
+
+// Source: http://stackoverflow.com/a/23238595/2079814
+holder = holder.filter(function(item, index) {
+    return holder.indexOf(item) == index;
+})
+
+holder = holder.filter(Boolean); // Strip out empty elements.
+
+var output_holder = "";
+
+for(var i = 0; i < holder.length; i++) {
+    output_holder += holder[i] + "\n";
+}
+output_holder = output_holder.substr(0, output_holder.length - 1); // Strip away the last newline.
+
+fs.writeFileSync(word_list, output_holder, "UTF-8");
+
+process.exit(0);
